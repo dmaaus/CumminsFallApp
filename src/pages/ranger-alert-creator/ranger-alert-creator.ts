d17286647ee323bcceaf7d538f0341cd21983319
Jonathan McClung <jonmcclung@gmail.com>
2018-01-27 21:00:10 -0600

    Implemented ranger sending of notification to users regarding park closing.

--- src/pages/ranger-alert-creator/ranger-alert-creator.ts
+++ src/pages/ranger-alert-creator/ranger-alert-creator.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {NotificationProvider} from "../../providers/notification/notification";
+import * as dateformat from 'dateformat';
 
 /**
  * Generated class for the RangerAlertCreatorPage page.
@@ -15,11 +17,12 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 })
 export class RangerAlertCreatorPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private notification: NotificationProvider) {
   }
 
   closePark() {
-    // TODO send a push notification to all users saying the park is closed.
+    let date = dateformat(Date.now(), 'mmm dS');
+    this.notification.post('Park Closing', `Cummins Falls is closed for the remainder of today, ${date}`);
   }
 
 }
